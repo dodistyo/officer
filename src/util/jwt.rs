75adc8b60cb9060b21f8bb5f9bafcb3029dcf62b
Dodi <github@ihc.id>
2025-02-23 17:29:22 +0700

    fix: decode token

--- src/util/jwt.rs
+++ src/util/jwt.rs
@@ -20,7 +20,6 @@ struct Jwks {
 struct Jwk {
     kid: String,
     kty: String,
-    alg: String,
     n: String,
     e: String,
 }
@@ -58,15 +57,43 @@ fn get_decoding_key(jwks: &Jwks, kid: &str) -> Option<DecodingKey> {
 // Validate a JWT token
 pub async fn validate_token(token: &str) -> Result<TokenData<Claims>, Box<dyn std::error::Error>> {
     let header = decode_header(token)?;
-    let kid = header.kid.ok_or("No kid in JWT header")?;
+    println!("Token header: {:?}", header);
+    let kid = match header.kid {
+        Some(kid) => kid,
+        None => {
+            println!("No kid in JWT header");
+            return Err("No kid in JWT header".into());
+        }
+    };
+    
     let jwks_url = get_jwks_url();
-    let jwks = fetch_jwks(&jwks_url).await?;
-    let decoding_key = get_decoding_key(&jwks, &kid).ok_or("No matching key found in JWKS")?;
+    let jwks = match fetch_jwks(&jwks_url).await {
+        Ok(jwks) => jwks,
+        Err(err) => {
+            println!("Failed to fetch JWKS: {:?}", err);
+            return Err("Failed to fetch JWKS".into());
+        }
+    };
+    
+    let decoding_key = match get_decoding_key(&jwks, &kid) {
+        Some(key) => key,
+        None => {
+            println!("No matching key found in JWKS for kid: {}", kid);
+            return Err("No matching key found in JWKS".into());
+        }
+    };
 
     let mut validation = Validation::new(Algorithm::RS256);
+    let oauth2_client_id = std::env::var("OAUTH2_CLIENT_ID").expect("OAUTH2_CLIENT_ID environment variable not set");
+    validation.set_audience(&[format!("{}", oauth2_client_id)]);
     validation.validate_exp = true;
 
-    let token_data = decode::<Claims>(token, &decoding_key, &validation)?;
-    println!("Token data: {:?}", token_data);
+    let token_data = match decode::<Claims>(token, &decoding_key, &validation) {
+        Ok(data) => data,
+        Err(err) => {
+            println!("Failed to decode token: {:?}", err);
+            return Err("Failed to decode token".into());
+        }
+    };
     Ok(token_data)
 }
